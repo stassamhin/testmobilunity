4a7450e5fb30329a01ca83e78f6056cd2b784be5
Stas Samgin <samgin.nitka@gmail.com>
2021-12-15 11:05:08 +0300

    chore: create repo

--- src/types/Book.ts
+++
@@ -1,9 +0,0 @@
-export interface Book {
-    book_author: string[]
-    book_pages: number
-    book_publication_city: string
-    book_publication_country: string
-    book_publication_year: number
-    book_title: string
-    id: number
-}
